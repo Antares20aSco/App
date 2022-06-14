e4973c04d8b1cdd2ac75987ad44917a8cfe03afd
Анастасия <102678798+Antares20aSco@users.noreply.github.com>
2022-06-14 23:08:32 +0300

    Add App

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '5451639922:AAGwEk3O8SCfyDr3f1gftkkGkJCIc-ifxZw'
+keys = {
+    'биткоин': 'BTC',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'эфириум': 'ETH',
+    'рубль': 'RUR',
+    'вона': 'KRW',
+    'лира': 'TRY',
+    'иена': 'JPY'
+}
